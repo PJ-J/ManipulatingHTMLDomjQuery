b5f6f648bbeba26a74c6e5d63683c4a38ac5cd91
PJ Jones <pj.jones4000.com>
2022-02-02 13:29:02 -0600

    finished #2 jQuery

--- jQuery/app.js
+++ jQuery/app.js
@@ -1,6 +1,11 @@
 $(function () {
   // Handler for .ready() called.
 
+  // add covalence number
+  let h31 = $("<h3>");
+  h31.html("1.");
+  h31.appendTo("body");
+
   //add button and click for alert
   let btn = $("<button>");
   btn.html("Alert");
@@ -8,6 +13,11 @@ $(function () {
 
   btn.click(function () {
     alert("you really did it");
-  })
+  });
 
+  //click button for alert containing text input
+  $("#boxBtn").click(function () {
+    alert($("#boxText").val());
+  });
+  
 });
